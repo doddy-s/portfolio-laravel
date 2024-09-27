bdeb480472c78982b347b4376734ceb8d71ebe43
doddy-s <doddy.s@students.amikom.ac.id>
2024-09-27 20:19:58 +0700

    chore: fix incorrect eslint rules on all files

--- resources/js/types/global.d.ts
+++ resources/js/types/global.d.ts
@@ -1,7 +1,7 @@
-import { PageProps as InertiaPageProps } from '@inertiajs/core';
-import { AxiosInstance } from 'axios';
-import { route as ziggyRoute } from 'ziggy-js';
-import { PageProps as AppPageProps } from './';
+import { PageProps as InertiaPageProps } from '@inertiajs/core'
+import { AxiosInstance } from 'axios'
+import { route as ziggyRoute } from 'ziggy-js'
+import { PageProps as AppPageProps } from './'
 
 declare global {
     interface Window {
@@ -9,7 +9,7 @@ declare global {
     }
 
     /* eslint-disable no-var */
-    var route: typeof ziggyRoute;
+    var route: typeof ziggyRoute
 }
 
 declare module '@inertiajs/core' {
